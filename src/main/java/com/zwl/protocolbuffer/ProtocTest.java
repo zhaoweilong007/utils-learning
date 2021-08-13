f9b0d26e46fbc94c5e40888f77f45c90f162b08c
赵伟龙 <374244818@qq.com>
2021-08-13 17:51:23 +0800

    更新protobuf测试

--- src/main/java/com/zwl/protocolbuffer/ProtocTest.java
+++ src/main/java/com/zwl/protocolbuffer/ProtocTest.java
@@ -1,14 +1,20 @@
 package com.zwl.protocolbuffer;
 
+import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
+import com.google.protobuf.Duration;
 import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Timestamp;
 import com.google.protobuf.util.JsonFormat;
 import com.zwl.protocolbuffer.AddressBook.Builder;
 import com.zwl.protocolbuffer.Person.PhoneNumber;
 import com.zwl.protocolbuffer.Person.PhoneType;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
@@ -87,5 +93,54 @@ public class ProtocTest {
 
   }
 
+
+  @Test
+  public void testField() {
+
+    ProtoDemo.Builder builder = ProtoDemo.newBuilder();
+    builder.setInt32(12);
+    builder.setInt64(123456L);
+    builder.setSint32(456);
+    builder.setSint64(789465789L);
+    builder.setUint32(-123);
+    builder.setUint64(-789456789L);
+    builder.setFloat(99.45F);
+    builder.setDouble(7.6D);
+    builder.setFixed32(456789);
+    builder.setFixed64(78974545421L);
+    builder.setSfixed32(45612);
+    builder.setSfixed64(979876465456L);
+    builder.setBool(true);
+    builder.setStr("hello protocol buffer");
+    builder.setBytes(ByteString.copyFrom("大家好啊快点哈健康和".getBytes(StandardCharsets.UTF_8)));
+    builder.setName("protobuf");
+    builder.setAge(18);
+    builder.putMap("张三", 23);
+    builder.putMap("李四", 30);
+    builder.addResults(Result.newBuilder()
+        .setCode(500)
+        .setMsg("服务器错误")
+        .setType(ResultType.fail)
+        .build());
+    builder.addDetails(Any.newBuilder().setValue(ByteString.copyFrom("dhajkhdjk".getBytes(
+        StandardCharsets.UTF_8))));
+    long second = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+    builder.setTimestamp(Timestamp.newBuilder().setSeconds(second).build());
+    builder.setDuration(Duration.newBuilder().setSeconds(second).build());
+
+    ProtoDemo protoDemo = builder.build();
+    log.info("protoDemo :{}", protoDemo);
+
+    ByteString byteString = protoDemo.toByteString();
+
+    ProtoDemo demo = null;
+    try {
+      demo = ProtoDemo.parseFrom(byteString.toByteArray());
+      log.info("demo map :{}", demo.getMapMap());
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
+    }
+
+  }
 
 }
